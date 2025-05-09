eb34ef20b8a36a611948ea0edd67785dbfb7ce7e
AgusSetti <asetti@mail.austral.edu.ar>
2025-05-09 10:14:33 -0300

    Update lists.py

--- lists.py
+++ lists.py
@@ -1,20 +1,69 @@
 # Replace the "ANSWER HERE" with your answer
 
 def remove_elements(list_to_remove_elements):
-    return "ANSWER HERE"  # Remove this line and implement
+    """remover los elementos que esten en la posición 1,5 y 6 de una lista que no sabemos el largo"""
+   # me fijo si la lista tiene al menos 7 elementos
+    if len(list_to_remove_elements)>=7:
+        #eleimino las posiciones que yo quiero
+        del list_to_remove_element[1]
+        del lista_to_remove_element[5]
+        del list_to_remove_element[6]
+    
+    if len(list_to_remove_element)==6:
+        del list_to_remove_element[1]
+        del list_to_remove_element[5]
+    
+    if len(list_to_remove_element) <= 5:
+        del list_to_remove_element[1]
+
+    return list_to_remove_element
 
 
 def add_elements(list_to_add_elements):
-    return "ANSWER HERE"  # Remove this line and implement
+    """agrego pink al principio de la lista y yellow al final de la lista"""
+    #primero agrego pink al principio de la lista
+    list_to_add_elements.instert(0,"pink")
+    #ahora agrego yellow al final de la lista
+    list_to_add_elements.append("yellow")
+
+    return list_to_add_elements
 
 
 def is_empty(list_to_check):
-    return "ANSWER HERE"  # Remove this line and implement
+    """que diga si la lista esta vacia o no"""
+    if len(list_to_check)==0:
+        return True
+    else:
+        return False
+    
 
 
 def check_lists(list_to_compare1, list_to_compare2):
-    return "ANSWER HERE"  # Remove this line and implement
+    """compara si el 3er elemento de las dos listas son iguales, pero no se el largo de la lista"""
+    #me fijo si la lista tiene al menos 3 elementos
+    if len(list_to_compare1) >= 3 and len(list_to_compare2) >= 3:
+        if list_to_compare1[2] == list_to_compare2[2]:
+            return True
+        else:
+            return False
 
 
-def list_of_lists(list_of_lists_to_modify):
-    return "ANSWER HERE"  # Remove this line and implement
+def list_of_lists(lista1 ,lista2, lista3):
+    """de la primera lista solo se queda con los primeros dos elementos. de la segunda lista solo se quede con los elementos entre el segundo y cuarto elemento.De la tercera lista solo se quede con los ultimos 2 elementos(no se sabe el largo de las listas internas)"""
+    #me fijo si la lista1 tiene al menos dos elementos
+    if len(lista1) >= 2:
+        lista1 = lista1[0:2]
+    elif len(lista1) < 2:
+        lista1 = lista1[0:1]
+        return lista1
+
+    #me fijo si la lista2 tiene al menos 4 elementos
+    if len(lista2) >= 4:
+        lista2 = lista2[1:4]
+    elif len(lista2) < 4:
+        lista2 = lista2[1:]
+        return lista2
+    #quiero los ultimos dos elementos de la lista3
+    lista3==lista3[-2:]
+    return lista3
+  
